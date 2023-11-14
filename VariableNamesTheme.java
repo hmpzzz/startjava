6d433698ad051317807299e7113b4bb35ec10b0f
Yakov <mymail163@gmail.com>
2023-11-14 23:58:01 +0400

    добавил чать первого ДЗ

--- VariableNamesTheme.java
+++ VariableNamesTheme.java
@@ -1,5 +1,44 @@
 public class VariableNamesTheme {
     public static void main(String[] args) {
-        System.out.println("Write once, run anywhere");
+
+        System.out.println("1. Разные переменные");
+
+        //Цифра
+        int number = 5;
+        System.out.println(number);
+        
+        //Сумма чисел
+        int sumNumbs = 12;
+        System.out.println(sumNumbs);
+
+        //Произведение цифр
+        int prodNumbs = 16;
+        System.out.println(prodNumbs);
+
+        //Максимальное число
+        int maxNumbs = 999;
+        System.out.println(maxNumbs);
+
+        //Количество десятков
+        int numbTens = 2;
+        System.out.println(numbTens);
+
+        //Вес собаки
+        int dogWeight = 18;
+        System.out.println(dogWeight);
+
+        //Исходное число
+        int originalNumb = 1;
+        System.out.println(originalNumb);
+
+        //Процент по вкладу
+        int interestDep = 11;
+        System.out.println(interestDep);
+
+        //&
+        char variableChar = '&';
+        System.out.println(variableChar);
+
+        
     }
 }
