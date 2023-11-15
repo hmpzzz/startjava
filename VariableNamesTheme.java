92543244541b4517f2758f62787fc29432205197
Yakov <mymail163@gmail.com>
2023-11-16 00:34:43 +0400

    Домашнее задание 1, часть 1

--- VariableNamesTheme.java
+++ VariableNamesTheme.java
@@ -6,7 +6,7 @@ public class VariableNamesTheme {
         //Цифра
         int number = 5;
         System.out.println(number);
-        
+
         //Сумма чисел
         int sumNumbs = 12;
         System.out.println(sumNumbs);
@@ -38,7 +38,84 @@ public class VariableNamesTheme {
         //&
         char variableChar = '&';
         System.out.println(variableChar);
-        
-        
+
+        //Код ошибки
+        int errorCode = 342;
+        System.out.println(errorCode);
+
+        //Тип сообщения
+        String messageType = "SMS";
+        System.out.println (messageType);
+
+        //Число нулей
+        int numZeros = 4;
+        System.out.println(numZeros);
+
+        //Уникальное число
+        int uniqNumber = 9;
+        System.out.println(uniqNumber);
+
+        //Случайное число
+        int rndNumb = 13;
+        System.out.println(rndNumb);
+
+        //Математическое выражение
+        String mathExpression = "4-3=1";
+        System.out.println(mathExpression);
+
+        //Выбор
+        String chs = "Select";
+        System.out.println(chs);
+
+        //Количество очков
+        int gameScore = 14;
+        System.out.println(gameScore);
+
+        //Максимальная длина строки
+        int maxLine = 80;
+        System.out.println(maxLine);
+
+        //Пункт меню
+        int menuItem = 3;
+        System.out.println(menuItem);
+
+        //Стоимость кофе на вынос
+        int costCoffe = 200;
+        System.out.println(costCoffe);
+
+        //Дата начала
+        String startDate = "15.11.2023";
+
+        //Окончание диапазона
+        int endRng = 999;
+        System.out.println(endRng);
+
+        //Полное имя работника месяца
+        String fullName = "Yakov";
+        System.out.println(fullName);
+
+        //Заголовок электронной книги
+        String ebookTitle = "Начало";
+        System.out.println(ebookTitle);
+
+        //Размер
+        int size = 48;
+        System.out.println(size);
+
+        //Вместимость
+        int capacity = 30;
+        System.out.println(capacity);
+
+        //Счетчик
+        int counter = 60;
+        System.out.println(counter);
+
+        //Путь до файла
+        String pathToFile = "/home/ya/Java/StartJava";
+        System.out.println(pathToFile);
+
+        //Количество чисел в каждой строке
+        int numbEachLine = 35;
+        System.out.println(numbEachLine);
     }
 }
