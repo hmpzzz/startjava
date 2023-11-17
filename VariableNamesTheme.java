5a29943ab744780f4aea531895741083e573cde1
Yakov <mymail163@gmail.com>
2023-11-17 07:53:32 +0400

    Доделал первую часть первого ДЗ

--- VariableNamesTheme.java
+++ VariableNamesTheme.java
@@ -5,117 +5,126 @@ public class VariableNamesTheme {
 
         //Цифра
         int number = 5;
-        System.out.println(number);
+        System.out.println("Цифра" + " = " + number);
 
         //Сумма чисел
         int sumNumbs = 12;
-        System.out.println(sumNumbs);
+        System.out.println("Сумма чисел" + " = " + sumNumbs);
 
         //Произведение цифр
         int prodNumbs = 16;
-        System.out.println(prodNumbs);
+        System.out.println("Произведение цифр" + " = " + prodNumbs);
 
         //Максимальное число
         int maxNumbs = 999;
-        System.out.println(maxNumbs);
+        System.out.println("Максимальное число" + " = " + maxNumbs);
 
         //Количество десятков
         int numbTens = 2;
-        System.out.println(numbTens);
+        System.out.println("Количество десятков" + " = " + numbTens);
 
         //Вес собаки
         int dogWeight = 18;
-        System.out.println(dogWeight);
+        System.out.println("Вес собаки" + " = " + dogWeight);
 
         //Исходное число
         int originalNumb = 1;
-        System.out.println(originalNumb);
+        System.out.println("Исходное число" + " = " + originalNumb);
 
         //Процент по вкладу
         int interestDep = 11;
-        System.out.println(interestDep);
+        System.out.println("Процент по вкладу" + " = " + interestDep);
 
         //&
         char variableChar = '&';
-        System.out.println(variableChar);
+        System.out.println("&" + " = " + variableChar);
 
         //Код ошибки
         int errorCode = 342;
-        System.out.println(errorCode);
+        System.out.println("Код ошибки" + " = " + errorCode);
 
         //Тип сообщения
         String messageType = "SMS";
-        System.out.println (messageType);
+        System.out.println ("Тип сообщения" + " = " + messageType);
 
         //Число нулей
         int numZeros = 4;
-        System.out.println(numZeros);
+        System.out.println("Число нулей" + " = " + numZeros);
 
         //Уникальное число
         int uniqNumber = 9;
-        System.out.println(uniqNumber);
+        System.out.println("Уникальное число" + " = " + uniqNumber);
 
         //Случайное число
         int rndNumb = 13;
-        System.out.println(rndNumb);
+        System.out.println("Случайное число" + " = "  + rndNumb);
 
         //Математическое выражение
         String mathExpression = "4-3=1";
-        System.out.println(mathExpression);
+        System.out.println("Математическое выражение" + " = " + mathExpression);
 
         //Выбор
         String chs = "Select";
-        System.out.println(chs);
+        System.out.println("Выбор" + " = " + chs);
 
         //Количество очков
         int gameScore = 14;
-        System.out.println(gameScore);
+        System.out.println("Количество очков" + " = " + gameScore);
 
         //Максимальная длина строки
         int maxLine = 80;
-        System.out.println(maxLine);
+        System.out.println("Максимальная длина строки" + " = " + maxLine);
 
         //Пункт меню
         int menuItem = 3;
-        System.out.println(menuItem);
+        System.out.println("Пункт меню" + " = " + menuItem);
 
         //Стоимость кофе на вынос
         int costCoffe = 200;
-        System.out.println(costCoffe);
+        System.out.println("Стоимость кофе на вынос" + " = " + costCoffe);
 
         //Дата начала
         String startDate = "15.11.2023";
+        System.out.println("Дата начала" + " = " + startDate);
 
         //Окончание диапазона
         int endRng = 999;
-        System.out.println(endRng);
+        System.out.println("Окончание диапазона" + " = " + endRng);
 
         //Полное имя работника месяца
         String fullName = "Yakov";
-        System.out.println(fullName);
+        System.out.println("Полное имя работника месяца" + " = " + fullName);
 
         //Заголовок электронной книги
         String ebookTitle = "Начало";
-        System.out.println(ebookTitle);
+        System.out.println("Заголовок электронной книги" + " = " + ebookTitle);
 
         //Размер
         int size = 48;
-        System.out.println(size);
+        System.out.println("Размер" + " = " + size);
 
         //Вместимость
         int capacity = 30;
-        System.out.println(capacity);
+        System.out.println("Вместимость" + " = " + capacity);
 
         //Счетчик
         int counter = 60;
-        System.out.println(counter);
+        System.out.println("Счетчик" + " = " + counter);
 
         //Путь до файла
-        String pathToFile = "/home/ya/Java/StartJava";
-        System.out.println(pathToFile);
+        String pathToFile = "/home/ya/Java/StartJava/VariableNamesTheme.java";
+        System.out.println("Путь до файла" + " = " + pathToFile);
 
         //Количество чисел в каждой строке
         int numbEachLine = 35;
-        System.out.println(numbEachLine);
+        System.out.println("Количество чисел в каждой строке" + " = " + numbEachLine);
+
+        System.out.println("");
+
+        System.out.println("1. boolean-переменные");
+
+        //Сотни равны?
+        boolean hundredsEqual = true;
+        System.out.println("Сотни равны?" + " = " + hundredsEqual);
     }
 }
